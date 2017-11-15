060caa2ee84b656d22c088c618825ca4f47bbea9
Israelem <israelem@gmail.com>
2017-11-15 18:51:20 +0100

    Corección nombre código parentesis

--- codes/2017-10-16-parentesis.py
+++ codes/2017-10-16-parentesis.py
@@ -1,15 +1,19 @@
 if __name__ == '__main__':
-    cadena = input()
-    lista_p = []
-    respuesta = "YES"
-    i = 0
-    while respuesta == "YES" and i < len(cadena):
-        if c in "{[(":
-            lista_p.append(c)
-        elif c in "}])" and not len(lista_p) or
-            c in "}])" and len(lista_p) and lista_p[len(lista_p) - 1] != c:
-            respuesta = "NO"
-        elif c in "}])" and len(lista_p) and lista_p[len(lista_p) - 1] == c:
-            lista_p = lista_p[ : len(lista_p)]
-        i += 1
+    cadena = [x for x in input() if x in "{}[]()"]
+    if len(cadena) % 2 != 0 or len(cadena) == 0:
+        respuesta = "NO"
+    else:
+        respuesta = "YES"
+        i = 0
+        while 0 < len(cadena) and respuesta == "YES":
+            if cadena[i] in "{[(":
+                i += 1
+            else:
+                if cadena[i - 1] + cadena[i] in '{}' or \
+                        cadena[i - 1] + cadena[i] in '[]' or \
+                        cadena[i - 1] + cadena[i] in '()':
+                    cadena = cadena[:i - 1] + cadena[i + 1:]
+                    i -= 1
+                else:
+                    respuesta = "NO"
     print(respuesta)

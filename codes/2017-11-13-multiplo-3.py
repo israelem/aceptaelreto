8bba0f42682c511ccaf3d8118fb98bdba93d6601
Israelem <israelem@gmail.com>
2017-11-30 21:30:32 +0100

    Corección código múltiplo 3 y código de voyayger (vacío)

--- codes/2017-11-13-multiplo-3.py
+++ codes/2017-11-13-multiplo-3.py
@@ -1,8 +1,16 @@
+def genera_numero(lenght):
+    res = ''
+    for n in range(1, lenght + 1):
+        res += str(n)
+    return int(res)
+
+
 if __name__ == "__main__":
     respuestas = []
     iteraciones = int(input())
     for i in range(iteraciones):
-        numero = int(input())
+        longitud = int(input())
+        numero = genera_numero(longitud)
         if numero % 3 == 0:
             respuestas.append('SI')
         else:

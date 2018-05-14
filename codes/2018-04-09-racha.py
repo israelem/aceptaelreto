c6ee503a390080ff25fe02414b10b16b4548a49d
israelem <israelem@gmail.com>
2018-05-14 19:06:33 +0200

    Código racha, entrada botín

--- codes/2018-04-09-racha.py
+++ codes/2018-04-09-racha.py
@@ -4,29 +4,15 @@ if __name__ == '__main__':
     for _ in range(numero_casos):
         dias = int(input())
         registro = [int(x) for x in input().split()]
-        ganancias = 0
-        intervalo = []
-        maximo = (0, [])
-        dia = 0
-        while dia < dias and -10000 < registro[dia] < 10000:
-            if registro[dia] > 0 and ganancias == 0:
-                intervalo.append(dia)
-                ganancias = registro[dia]
-            elif registro[dia] > 0:
-                ganancias += registro[dia]
-            elif ganancias > 0 and not intervalo == []:
-                intervalo.append(dia - 1)
-                if ganancias > maximo[0] or (ganancias == maximo[0] and
-                                             intervalo[1] - intervalo[0] < maximo[1][1] - maximo[1][0]):
-                    maximo = (ganancias, intervalo)
-                intervalo = []
-                ganancias = 0
-            dia += 1
-        if ganancias > 0 and not intervalo == []:
-                intervalo.append(dia - 1)
-                if ganancias > maximo[0] or (ganancias == maximo[0] and
-                                             intervalo[1] - intervalo[0] < maximo[1][1] - maximo[1][0]):
-                    maximo = (ganancias, intervalo)
+        ganancias = []
+        for inicio in range(dias):
+            for fin in range(inicio, dias):
+                ganancias.append((sum(registro[inicio:fin + 1]), [inicio + 1, fin + 1]))
+        maximo = ganancias[0]
+        for ganancia in ganancias[1:]:
+            if ganancia[0] > maximo[0] or (ganancia[0] == maximo[0] and (ganancia[1][1] - ganancia[1][0]) <
+                                           (maximo[1][1] - maximo[1][1])):
+                maximo = ganancia
         respuestas.append(maximo[1])
         for respuesta in respuestas:
-            print(respuesta[0]+1, respuesta[1]+1)
+            print(respuesta[0], respuesta[1])

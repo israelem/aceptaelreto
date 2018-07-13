20eaadff641bcb344a7b3f34eb002fb0f8ff450e
israelem <israelem@gmail.com>
2018-07-13 20:12:49 +0200

    Código kaprekar

--- codes/2018-06-04-kaprekar.py
+++ codes/2018-06-04-kaprekar.py
@@ -1,5 +1,18 @@
 from functools import reduce
 
+
+def cadena_a_numero(cadena, revés=False):
+    lista = sorted([c for c in cadena], reverse=revés)
+    número = int(reduce(lambda x, y: x + y, lista))
+    return número
+
+
+def rellenar_ceros(cadena):
+    if len(cadena) < 4:
+        cadena += '0' * (4 - len(cadena))
+    return cadena
+
+
 if __name__ == '__main__':
     soluciones = []
     número_casos = int(input())
@@ -11,7 +24,12 @@ if __name__ == '__main__':
         elif reduce(lambda x, y: x and y, list(map(lambda x: número[0] == x, número))):
             solución = 8
         else:
-            pass
+            solución = 0
+            while número != '6174':
+                mayor = cadena_a_numero(número, True)
+                menor = cadena_a_numero(número)
+                número = rellenar_ceros(str(mayor - menor))
+                solución += 1
         soluciones.append(solución)
     for solución in soluciones:
         print(solución)

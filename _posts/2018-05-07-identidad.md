2edf6fe6915e3cb1df9ddd1f829ab3c02ab8ea87
israelem <israelem@gmail.com>
2018-07-18 12:13:16 +0200

    Código hamburguesquin y correcciones

--- _posts/2018-05-07-identidad.md
+++ _posts/2018-05-07-identidad.md
@@ -46,7 +46,24 @@ NO
 # Solución propuesta
 
 ``` python
+import numpy as np
 
+respuestas = []
+tamaño = (int(input()))
+while tamaño:
+    matriz = []
+    for _ in range(tamaño):
+        matriz.append([int(x) for x in input().split()])
+    matriz = np.array(matriz, dtype=int)
+    identidad = np.identity(tamaño, int)
+    if np.array_equal(matriz, identidad):
+        respuestas.append('SI')
+    else:
+        respuestas.append('NO')
+    tamaño = int(input())
+
+for respuesta in respuestas:
+    print(respuesta)
 ```
 
 [Enlace del código](https://github.com/israelem/aceptaelreto/blob/master/codes/2018-05-7-identidad.py)
--- codes/2018-01-01-hamburguesquin.py
+++ codes/2018-01-01-hamburguesquin.py
@@ -4,11 +4,64 @@ class Intervalo:
         self.centro = centro
         self.radio = radio
 
+    def extremo_izquierdo(self):
+        return self.centro - self.radio
+
+    def extremo_derecho(self):
+        return self.centro + self.radio
+
+    def __eq__(self, intervalo):
+        return self.centro == intervalo.centro and self.radio == intervalo.radio
+
+    def __ne__(self, intervalo):
+        return self.centro != intervalo.centro and self.radio != intervalo.radio
+
+    def __le__(self, intervalo):
+        return self.extremo_izquierdo() <= intervalo.extremo_izquierdo()
+
+    def __lt__(self, intervalo):
+        return self.extremo_izquierdo() < intervalo.extremo_izquierdo()
+
+    def __ge__(self, intervalo):
+        return self.extremo_izquierdo() >= intervalo.extremo_izquierdo()
+
+    def __gt__(self, intervalo):
+        return self.extremo_izquierdo() > intervalo.extremo_izquierdo()
+
+    def __str__(self):
+        extremo_izquierdo = self.extremo_izquierdo()
+        extremo_derecho = self.extremo_derecho()
+        return f'[{extremo_izquierdo}, {extremo_derecho}]'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.centro}, {self.radio})'
+
 
 if __name__ == '__main__':
+    soluciones = []
     longitud, restaurantes = [int(x) for x in input().split()]
     while longitud and restaurantes:
         lista_restaurantes = []
         for _ in range(restaurantes):
             centro, radio = [int(x) for x in input().split()]
             lista_restaurantes.append(Intervalo(centro, radio))
+        lista_restaurantes.sort()
+        if lista_restaurantes[0].extremo_izquierdo() > 1:
+            soluciones.append(-1)
+        else:
+            eliminables = 0
+            posición = 1
+            while posición < len(lista_restaurantes) and \
+                    lista_restaurantes[posición - 1].extremo_derecho() >= lista_restaurantes[posición].extremo_izquierdo():
+                if posición < len(lista_restaurantes) - 1 and \
+                        lista_restaurantes[posición].extremo_derecho() >= lista_restaurantes[posición + 1].extremo_izquierdo():
+                    eliminables += 1
+                posición += 1
+            if posición == len(lista_restaurantes) and lista_restaurantes[posición - 1].extremo_derecho() >= longitud:
+                soluciones.append(eliminables)
+            else:
+                soluciones.append(-1)
+        longitud, restaurantes = [int(x) for x in input().split()]
+
+    for solucion in soluciones:
+        print(solucion)

a5274ab87cfec5adac9084dedc4bd7d36447b90a
israelem <israelem@gmail.com>
2018-07-24 13:47:08 +0200

    Código Jaén

--- codes/2018-06-25-jaen.py
+++ codes/2018-06-25-jaen.py
@@ -1,17 +1,28 @@
 import numpy as np
+from itertools import product
 
 
-def calcular_camino_mas_largo(mapa, inicio=(0, 0)):
-    if not (inicio[0] < mapa.shape[0] and inicio[1] < mapa.shape[1]):
+def vacío(mapa):
+    vacío = True
+    for fila, columna in list(product(range(mapa.shape[0]), range(mapa.shape[1]))):
+        vacío = vacío and mapa[fila, columna] == ' '
+    return vacío
+
+
+def calcular_contiguos(mapa, lista_puntos, inicio):
+    if mapa[inicio[0], inicio[1]] == ' ':
+        lista_puntos.remove(inicio)
         return 0
     elif mapa[inicio[0], inicio[1]] == '#':
         mapa[inicio[0], inicio[1]] = ' '
-        return 1 + calcular_camino_mas_largo(mapa, [inicio[0] + 1, inicio[1]]) + \
-               calcular_camino_mas_largo(mapa, [inicio[0], inicio[1] + 1])
-    else:
-        return max(calcular_camino_mas_largo(mapa, [inicio[0] + 1, inicio[1]]),
-                   calcular_camino_mas_largo(mapa, [inicio[0], inicio[1] + 1]))
-
+        lista_puntos.remove(inicio)
+        contiguos_fila = 0
+        contiguos_columna = 0
+        if [inicio[0] + 1, inicio[1]] in lista_puntos:
+            contiguos_fila = calcular_contiguos(mapa, lista_puntos, [inicio[0] + 1, inicio[1]])
+        if [inicio[0], inicio[1] + 1] in lista_puntos:
+            contiguos_columna = calcular_contiguos(mapa, lista_puntos, [inicio[0], inicio[1] + 1])
+        return 1 + contiguos_fila + contiguos_columna
 
 if __name__ == '__main__':
     soluciones = []
@@ -20,7 +31,13 @@ if __name__ == '__main__':
         mapa = np.empty([filas, columnas], str)
         for fila in range(filas):
             mapa[fila, :] = [c for c in input()]
-        soluciones.append(calcular_camino_mas_largo(mapa))
+        lista_parcelas = []
+        lista_puntos = [[x, y] for x, y in
+                        list(product(range(mapa.shape[0]),
+                                     range(mapa.shape[1])))]
+        while not vacío(mapa):
+            lista_parcelas.append(calcular_contiguos(mapa, lista_puntos, lista_puntos[0]))
+        soluciones.append(max(lista_parcelas))
         filas, columnas = [int(x) for x in input().split()]
     for solución in soluciones:
         print(solución)
